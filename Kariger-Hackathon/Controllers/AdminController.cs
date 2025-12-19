a72c337305cb733ac52c662275bc4f71c87f1977
mannanbaig <mannanbaig14@gmail.com>
2025-12-19 10:44:50 +0500

    Added Frontened

---
+++ Kariger-Hackathon/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kariger_Hackathon.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Reviews()
+        {
+            return View();
+        }
+    }
+}
---
+++ Kariger-Hackathon/Controllers/ServiceProviderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kariger_Hackathon.Controllers
+{
+    public class ServiceProviderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ServiceProvider()
+        {
+            return View();
+        }
+        public IActionResult Bookings()
+        {
+            return View();
+        }
+
+    }
+}
